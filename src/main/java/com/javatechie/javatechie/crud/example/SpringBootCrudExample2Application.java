d38eb4c0ad3d2cf36012596965bc59193f8cdccb
Nuri Korganci <nkorganci@gmail.com>
2022-03-02 16:34:41 -0600

    Initial commit

---
+++ src/main/java/com/javatechie/javatechie/crud/example/SpringBootCrudExample2Application.java
@@ -0,0 +1,17 @@
+package com.javatechie.javatechie.crud.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudExample2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudExample2Application.class, args);
+		System.out.println("Hello 1st page");
+		index();
+	}
+	public static String index(){
+		return "index.html";  // Why it shows it in localport:8080 ?
+	}
+}
